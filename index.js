a72fc51c99428bddecb14b4f4e85f20a0e0d8cc4
JennyTLam <jennylam2021@u.northwestern.edu>
2021-01-04 13:32:23 -0600

    working drum sounds

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ function playAudio(sound){
     var audio = new Audio(sound);
     audio.play()
 }
+
+const w = document.getElementById("w");
+const a = document.getElementById("a");
+const s = document.getElementById("s");
+const d = document.getElementById("d");
+const j = document.getElementById("j");
+const k = document.getElementById("k");
+const l = document.getElementById("l");
+
+w.addEventListener("click", () => playAudio("./sounds/tom-1.mp3" ));
+a.addEventListener("click", () => playAudio("./sounds/tom-2.mp3"));
+s.addEventListener("click", () => playAudio("./sounds/tom-3.mp3"));
+d.addEventListener("click", () => playAudio("./sounds/tom-4.mp3"));
+j.addEventListener("click", () => playAudio("./sounds/snare.mp3"));
+k.addEventListener("click", () => playAudio("./sounds/crash.mp3"));
+l.addEventListener("click", () => playAudio("./sounds/kick-bass.mp3"));
